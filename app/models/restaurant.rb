8604957423bc3299a1b33553b12f99950dc11106
Harrison Kim <namo918@gmail.com>
2017-11-15 12:01:40 +0800

    fire drill

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+    validates :name, uniqueness: true
+    validates :address, :name, presence: true
+    validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content
+  validates :rating, :content, presence: true, inclusion: { :in [1, 2, 3, 4, 5], allow_nil: false }
+
 end
