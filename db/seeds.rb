ea24c8f8c60de3b8b73d9ba388dd37690ba8039e
Harrison Kim <namo918@gmail.com>
2017-11-15 15:31:57 +0800

    seeded 10 restaurants using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+type = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times do
+  Restaurant.create(name: Faker::Company.name, address: Faker::Address.street_address, category: type.sample)
+  puts "created a restaurant"
+end
